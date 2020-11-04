5b61f8e464f3a93947b4e1f663a6dbdaea22e95a
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-04 17:32:28 +0530

    Code for the calculation of the daily employee wage is added

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,12 +1,20 @@
 #!/bin/bash 
 echo "Welcome to the Employee Wage program"
-emppresent=1
-empabsent=0
+WagePerHr=20
+DailyEmpWage=0
+present=1
+absent=0
+empHr=0
 empcheck=$(($RANDOM%2))
-if [[ $empcheck -eq 1 ]]
+if [[ $empcheck -eq $present ]] 
 then
-echo "Employee is present"
-elif [[ $empcheck -eq 0 ]]
+echo "Employee is present."
+empHr=8
+elif [[ $empcheck -eq $absent ]]
 then
 echo "Employee is absent"
+empHr=0
 fi
+
+DailyEmpWage=$(($empHr*$WagePerHr))
+echo "Daily Employee Wage=" $DailyEmpWage
