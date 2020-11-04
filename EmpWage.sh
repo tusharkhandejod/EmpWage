ba819ab9a0c59a075d39ea62890b41f2132bcf41
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-04 17:29:16 +0530

    Code for Employee Attendance is added

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,2 +1,12 @@
 #!/bin/bash 
 echo "Welcome to the Employee Wage program"
+emppresent=1
+empabsent=0
+empcheck=$(($RANDOM%2))
+if [[ $empcheck -eq 1 ]]
+then
+echo "Employee is present"
+elif [[ $empcheck -eq 0 ]]
+then
+echo "Employee is absent"
+fi
