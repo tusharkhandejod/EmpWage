96016ac5c9b679ea2661e3a9036f72c4b29795a2
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-04 18:02:19 +0530

    Code for storing Day, daily and total monthly wages is added

--- EmpWage.sh
+++ EmpWage.sh
@@ -1,15 +1,16 @@
-#!/bin/bash 
+#!/usr/local/bin/bash 
 echo "Welcome to the Employee Wage program"
 WagePerHr=20
 DailyEmpWage=0
 declare -A DailyWage;
 totalMonthWage=0
+declare -A totalMonthlyWage;
 Fulltime=1
 Halftime=2
 absent=0
 empHr=0
 totalworkingdays=0
-echo "Day" "DailyWage" "Total Monthly Wage"
+echo "Day"   "DailyWage"    "Total Monthly Wage"
 while [[ $totalworkingdays -lt 20 && $totalworkinghours -lt 100 ]]
 do
 empcheck=$(($RANDOM%3))
@@ -31,6 +32,7 @@ esac
 DailyEmpWage=$(($empHr*$WagePerHr))
 DailyWage["Day " $totalworkingdays]=$DailyEmpWage
 totalMonthWage=$(($totalMonthWage+$DailyEmpWage))
+totalMonthlyWage["Day " $totalworkingdays]=$totalMonthWage
 echo "$totalworkingdays" "$DailyEmpWage" "$totalMonthWage"
 done
 echo "Total Monthly Wage=" $totalMonthWage
