4acbe134d4866ca3bebe908ec90352fdb54c8b5f
tusharkhandejod <tusharkhandejod@gmail.com>
2020-11-04 17:35:23 +0530

    Code for the calculation of parttime wage is added

--- EmpWage.sh
+++ EmpWage.sh
@@ -2,14 +2,19 @@
 echo "Welcome to the Employee Wage program"
 WagePerHr=20
 DailyEmpWage=0
-present=1
+fulltime=1
+parttime=2
 absent=0
 empHr=0
-empcheck=$(($RANDOM%2))
-if [[ $empcheck -eq $present ]] 
+empcheck=$(($RANDOM%3))
+if [[ $empcheck -eq $fulltime ]]
 then
-echo "Employee is present."
+echo "Employee is present for full time"
 empHr=8
+elif [[ $empcheck -eq $parttime ]]
+then
+echo "Employee is present for part time"
+empHr=4
 elif [[ $empcheck -eq $absent ]]
 then
 echo "Employee is absent"
